a0899160519bc1b48989dbe5cde5fc1ed2e0d008
Gimbol4 <Shishkanov.yd@gmail.com>
2026-04-29 18:44:51 +0300

    solve 2 test

--- test02/logic.h
+++ test02/logic.h
@@ -1,1 +1,4 @@
-#pragma once
+
+int* get_all_local_minimums(int* array, int size);
+int sum_local_minimums(int* array, int size);
+int count_local_minimums(int* array, int size);
--- test02/main.cpp
+++ test02/main.cpp
@@ -1,4 +1,5 @@
 #include "util.h"
+#include "logic.h"
 
 int main() {
 	int size;
@@ -16,8 +17,14 @@ int main() {
 
 	print("Array: " + convert(array, size) + "\n");
 
-	//logic
+	int count = count_local_minimums(array, size);
+	int* minimums = get_all_local_minimums(array, size);
+
+	print("All local minimums: " + convert(minimums, count) + "\n");
+	print("sum of local minimums is " 
+		+ to_string(sum_local_minimums(minimums, count)) + ".\n");
 
 	remove(array);
+
 	return 0;
 }
